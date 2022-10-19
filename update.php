f65563be625040463708054bbde599628495a239
MironFoerster <miron.foerster@gmx.de>
2022-10-19 19:00:05 +0200

    start update php

---
+++ update.php
@@ -0,0 +1,41 @@
+<?php
+
+$server_name='localhost';
+$username='root';
+$password='';
+$database_name='sqlgarden';
+
+$conn=mysqli_connect($server_name,$username,$password,$database_name);
+if (!$conn) {
+   die('Connection failed: ' . $conn->connect_error);
+}
+foreach ($_REQUEST['data'] as $row) {
+  switch ($_REQUEST["table_name"]) {
+    case "awards":
+      break;
+    case "tiles":
+      $sql = "
+      IF EXISTS (SELECT 1 FROM".$_REQUEST['table_name']."WHERE UniqueColumn = 'Something')
+      BEGIN
+          UPDATE Tbl 
+          SET ...
+          WHERE UniqueColumn = 'Something';
+      END
+      ELSE
+      BEGIN
+          INSERT INTO Tbl
+          SELECT ...
+      END";
+      break;
+    case "builts":
+      break;
+    case "gamestates":
+      break;
+    default:
+      
+  }
+  $result=mysqli_query($conn, $sql);
+}
+
+echo '["updated"]';
+?>
